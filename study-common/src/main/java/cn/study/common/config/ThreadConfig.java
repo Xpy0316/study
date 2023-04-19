ccc8cebdbe49fdbab9a001f5b4ab7de6b7613d7f
“xpy” <975615887@qq.com>
2023-04-19 11:01:19 +0800

    1

---
+++ study-common/src/main/java/cn/study/common/config/ThreadConfig.java
@@ -0,0 +1,11 @@
+package cn.study.common.config;
+
+/**
+ * @ClassName ThreadConfig
+ * @Description TODO
+ * @Author v-xupengyuan
+ * @Date 2023/4/19 11:00
+ * @Version 1.0
+ */
+public class ThreadConfig {
+}
